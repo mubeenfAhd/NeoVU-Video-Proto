a12226383f632e9e89b8c8e45c74e3b64cc522ec
mubeenfahd <mah@neo-vu.com>
2021-08-07 13:06:07 +0500

    synced video controls
    synced play, pause and seekbar controls

--- Assets/ProductFiles/Scripts/VideoManager.cs
+++ Assets/ProductFiles/Scripts/VideoManager.cs
@@ -7,19 +7,19 @@ using UnityEngine.UI;
 using UnityEngine.Video;
 using Hashtable = ExitGames.Client.Photon.Hashtable;
 
-public class VideoManager : MonoBehaviourPunCallbacks, IPointerDownHandler, IPointerUpHandler
+public class VideoManager : MonoBehaviourPunCallbacks
 {
     public VideoPlayer videoPlayer;
     public VideoClip[] videoClips;
 
     public Button play_Button;
-    public Button pause_Button;
+    //public Button pause_Button;
     public Slider videoSeek;
 
-    float videoSeekTime = 0.0f;
-    long videoFrame = 0;
+    //float videoSeekTime = 0.0f;
+    //long videoFrame = 0;
 
-    bool slide = false;
+    //bool slide = false;
 
     private List<int> userList;
 
@@ -39,11 +39,6 @@ public class VideoManager : MonoBehaviourPunCallbacks, IPointerDownHandler, IPoi
         {
             userList.Add(p.ActorNumber);
         }
-
-        //PhotonNetwork.Instantiate("VideoPlayerPrefab", new Vector3(0,0,0), Quaternion.identity);
-
-        //videoPlayer = GameObject.FindObjectOfType<VideoPlayer>();
-        //view = GameObject.FindObjectOfType<PhotonView>();
     }
     void Start()
     {
@@ -52,94 +47,60 @@ public class VideoManager : MonoBehaviourPunCallbacks, IPointerDownHandler, IPoi
         if (PhotonNetwork.IsMasterClient)
         {
             play_Button.interactable = true;
-            pause_Button.interactable = true;
+            //pause_Button.interactable = true;
             videoSeek.interactable = true;
         }
-
-        Hashtable props = new Hashtable
-            {
-                {VideoDetail.USER_LOADED_LEVEL, true}
-            };
-        PhotonNetwork.LocalPlayer.SetCustomProperties(props);
     }
 
-    private void Update()
-    {
-        if (!slide)
+    public void VideoControls() {
+        if (videoPlayer.isPlaying)
         {
-            if (videoPlayer != null)
-            {
-                videoSeek.value = (float)videoPlayer.frame / (float)videoPlayer.frameCount;
-                videoSeekTime = videoSeek.value;
-
-                if (PhotonNetwork.IsMasterClient)
-                {
-                    sseekValu[VideoDetail.VIDEO_SEEK] = videoSeekTime;
-                }
-            }
+            view.RPC("ChangeVideoState", RpcTarget.AllViaServer, "pause");
+        }
+        else if (videoPlayer.isPaused)
+        {
+            view.RPC("ChangeVideoState", RpcTarget.AllViaServer, "play");
         }
     }
 
-    //static float SyncVideoSeek()
-    //{
-    //    Player p;
-    //    object seek;
-    //    if (p.CustomProperties.TryGetValue(VideoDetail.VIDEO_SEEK, out seek))
-    //    {
-    //        return (float)seek;
-    //    }
-
-    //    return 0;
-    //    //if (PhotonNetwork.LocalPlayer.CustomProperties.TryGetValue(VideoDetail.VIDEO_SEEK, out seek))
-    //    //{
-    //    //    PhotonNetwork.LocalPlayer.SetCustomProperties(new Hashtable { { VideoDetail.VIDEO_SEEK, ((float)seek = videoSeekTime) } });
-    //    //}
-    //}
-
-    public void VideoControls (string value){
-        if (value.Contains("Play"))
-        {
-            videoPlayer.Play();
-            Debug.Log("h");
-            //if (!PhotonNetwork.IsMasterClient)
-            if (!view.IsMine)
-            {
-                Debug.Log("hhh");
-                videoPlayer.Play();
-            }
-
-        }
-        else
-        {
-            videoPlayer.Pause();
-
-            //if (!PhotonNetwork.IsMasterClient)
-            if (!view.IsMine)
-            {
-                Debug.Log("jjj");
-                videoPlayer.Pause();
-            }
-        }
-    }
-
-    public void OnPointerDown(PointerEventData eventData)
+    public void seekControl(float val)
     {
-        slide = true;
+        view.RPC("SeekBarPositionChanged", RpcTarget.Others, val);
     }
 
-    public void OnPointerUp(PointerEventData eventData)
+    public void VideoFrameChanged(float frame)
     {
-        float frame = (float)videoSeek.value * (float)videoPlayer.frameCount;
-        videoPlayer.frame = (long)frame;
-        videoFrame = videoPlayer.frame;
-
-        slide = false;
+        view.RPC("FrameChanged", RpcTarget.Others, frame);
     }
-
-
-   
 
     #region PUN CALLBACKS
+    [PunRPC]
+    public void SeekBarPositionChanged(float seekVal, PhotonMessageInfo info)
+    {
+        videoSeek.value = seekVal;
+    }
+
+    [PunRPC]
+    public void FrameChanged(float frame, PhotonMessageInfo info)
+    {
+        videoPlayer.frame = (long)frame;
+    }
+
+    [PunRPC]
+    public void ChangeVideoState(string value, PhotonMessageInfo info)
+    {
+        if (value.Contains("pause"))
+        {
+            videoPlayer.Pause();
+            play_Button.GetComponentInChildren<Text>().text = "Play";
+        }
+        else if (value.Contains("play"))
+        {
+            videoPlayer.Play();
+            play_Button.GetComponentInChildren<Text>().text = "Pause";
+        }
+    }
+
     public override void OnMasterClientSwitched(Player newMasterClient)
     {
         if (PhotonNetwork.LocalPlayer.ActorNumber == newMasterClient.ActorNumber)
@@ -147,21 +108,8 @@ public class VideoManager : MonoBehaviourPunCallbacks, IPointerDownHandler, IPoi
             if (PhotonNetwork.IsMasterClient)
             {
                 play_Button.interactable = true;
-                pause_Button.interactable = true;
+                //pause_Button.interactable = true;
                 videoSeek.interactable = true;
-            }
-        }
-    }
-
-    public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
-    {
-        if (PhotonNetwork.LocalPlayer.ActorNumber == targetPlayer.ActorNumber)
-        {
-            if (!PhotonNetwork.IsMasterClient)
-            {
-                Debug.Log("ddd");
-                videoSeek.value = videoSeekTime;
-                videoPlayer.frame = videoFrame;
             }
         }
     }
---
+++ Assets/SeekBarControl.cs
@@ -0,0 +1,50 @@
+﻿using Photon.Pun;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class SeekBarControl : MonoBehaviourPunCallbacks, IPointerDownHandler, IPointerUpHandler
+{
+    public VideoManager vManager;
+
+    bool slide = false;
+
+    public VideoPlayer videoPlayer;
+    public Slider videoSeek;
+
+    public PhotonView view;
+
+    long videoFrame = 0;
+    float videoSeekTime = 0.0f;
+
+    private void Update()
+    {
+        if (!slide)
+        {
+            float val = (float)videoPlayer.frame / (float)videoPlayer.frameCount;
+            if (PhotonNetwork.IsMasterClient)
+            {
+                videoSeek.value = val;
+            }   
+            
+            vManager.seekControl(val);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        slide = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        float frame = (float)videoSeek.value * (float)videoPlayer.frameCount;
+        if (PhotonNetwork.IsMasterClient)
+        {
+            videoPlayer.frame = (long)frame;
+        }
+        
+        vManager.VideoFrameChanged(frame);
+        slide = false;
+    }
+}
